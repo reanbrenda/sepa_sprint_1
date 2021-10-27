41dc7940ca3c460278b5224d085cb585df4a7baf
reanbrenda <mukindiabrenda67@gmail.com>
2021-10-27 15:59:04 +0300

    adding purchase fuction

--- customerproductsystem.py
+++ customerproductsystem.py
@@ -5,6 +5,8 @@ def insert_customer():
     c_id = input("Enter a unique id:")
     dict = {"name": c_name, "address": c_address, "customer_id": c_id}
     return dict
+
+
 def delete_customer(customer_list):
     c_id = input("Enter the customer Id:")
     for i in range(len(customer_list)-1):
@@ -12,6 +14,8 @@ def delete_customer(customer_list):
         if id==c_id:
             customer_list.remove(customer_list[i])
     return customer_list
+
+
 def update_customer_data(customer_list):
     c_id = input("Enter the customer Id:")
     cchoice=int(input("enter your choice"))
@@ -26,6 +30,7 @@ def update_customer_data(customer_list):
                 customer_list[i]['address']=address
     return customer_list
 
+
 def insert_product():
     p_name = input("Enter the product name:")
     p_amount = int(input("Enter the number of product:"))
@@ -34,6 +39,7 @@ def insert_product():
     dict = {"name": p_name, "amount": p_amount, "price": p_price,"product_id":p_id}
     return dict
 
+
 def delete_product(product_list):
     p_id = input("Enter the product Id:")
     for i in range(len(product_list) - 1):
@@ -41,6 +47,7 @@ def delete_product(product_list):
         if id == p_id:
             product_list.remove(product_list[i])
     return product_list
+
 
 def update_product_data(product_list):
     p_id = input("Enter the product Id:")
@@ -58,6 +65,8 @@ def update_product_data(product_list):
                 price = float(input("enter the new price"))
                 product_list[i]['price'] = price
     return product_list
+
+
 def search_product(product_list):
     p_id = input("Enter the product Id:")
     for i in range(len(product_list)):
@@ -69,19 +78,44 @@ def search_product(product_list):
     dict={"product_name":name,"product_amount":amount,"product_price":price}
     return dict
 
-def purchase(product_list):
+
+def amount_purchased(product_data_list):
+    productamount=[]
+    total=0
+    c_id = input("Enter the customer Id:")
+    for i in range(len(product_data_list)):
+        id = product_data_list[i]['customerid']
+        if id==c_id:
+            cname=product_data_list[i]["customer name"]
+            price = product_data_list[i]["product_price"]
+            name=product_data_list[i]["product_name"]
+            amount=product_data_list[i]["productamount"]
+            tprice=price*amount
+            product={"product_name":name,"amount":amount}
+            productamount.append(product)
+            total=total+tprice
+    print(cname,"bought",productamount,"at a total price of",total)
+
+
+def purchase(customer_list,product_list,):
     p_id = input("Enter the product Id:")
     c_id=input("enter the customer id")
-    c_amount=int(input("enter the amount purchased"))
-    for i in range(len(product_list)):
-        if p_id==product_list[i]['product_id']:
-            if c_amount<=product_list[i]['amount']:
-                amount=product_list[i]['amount']-c_amount
-                product_list[i]['amount']=amount
-            else:
-                print("out of stock")
-        dict = {"cutomerid": c_id, "productid": p_id, "productamount": c_amount}
-
+    c_amount = int(input("enter the amount purchased"))
+    for i in range(len(customer_list)):
+        name = customer_list[i]['name']
+        if c_id ==customer_list[i]['customer_id']:
+            for i in range(len(product_list)):
+                print(product_list)
+                if p_id==product_list[i]['product_id']:
+                    price = product_list[i]["price"]
+                    pname = product_list[i]["name"]
+                    if c_amount<=product_list[i]['amount']:
+                        amount=product_list[i]['amount']-c_amount
+                        product_list[i]['amount']=amount
+                    else:
+                        print("out of stock")
+    dict = {"customerid": c_id, "customer name": name, "productid": p_id, "product_name": pname,
+                        "productamount": c_amount, "product_price": price}
     return dict
 
 
@@ -90,7 +124,24 @@ if __name__ == '__main__':
 
         customer_list=[]
         product_list=[]
-        product=[]
+        product_data_list=[]
+        customer = open("customer_info.txt", "r")
+        fcustomer = open("customer_info.txt", "a")
+        for s in customer:
+            data = ast.literal_eval(s)
+            customer_list.append(data)
+        product = open("product_info.txt", "r")
+        fproduct = open("product_info.txt", "a")
+
+        for s in product:
+            data = ast.literal_eval(s)
+            product_list.append(data)
+        pdata=open("customerproductdata.txt","r")
+        fdata = open("customerproductdata.txt", "a")
+        for s in pdata:
+            data=ast.literal_eval(s)
+            product_data_list.append(data)
+
         print("""
                     What would you want  to update
                      1.Customer
@@ -106,11 +157,6 @@ if __name__ == '__main__':
                           4.list customers
                           """)
             option=int(input("enter the operation you wish to perform"))
-            customer = open("customer_info.txt", "r")
-            fcustomer = open("customer_info.txt", "a")
-            for s in customer:
-                data = ast.literal_eval(s)
-                customer_list.append(data)
             if option==1:
                 print(insert_customer(),file=fcustomer)
             elif option == 2:
@@ -134,13 +180,9 @@ if __name__ == '__main__':
                                   4. purchase product
                                   5. lists products
                                   6. search product
+                                  7.list customer name and total amount spent
                                   """)
             option = int(input("enter the operation you wish to perform"))
-            product = open("product_info.txt", "r")
-            fproduct = open("product_info.txt", "a")
-            for s in product:
-                data = ast.literal_eval(s)
-                product_list.append(data)
             if option==1:
                 print(insert_product(),file=fproduct)
             elif option==2:
@@ -155,15 +197,15 @@ if __name__ == '__main__':
                         print(j[i],file=myfile)
 
             elif option==4:
-               j = purchase(product_list)
-               product.append(j)
+               j = purchase(customer_list,product_list)
+               print(j,file=fdata)
                with open('product_info.txt', "w") as myfile:
                    for i in range(len(product_list)):
                        print(product_list[i], file=myfile)
-
             elif option==5:
                 print(product_list)
             elif option==6:
                 print(search_product(product_list))
-
+            elif option==7:
+                amount_purchased(product_data_list)
 
